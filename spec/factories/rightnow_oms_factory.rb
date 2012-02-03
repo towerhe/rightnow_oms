c3a5d41e9fc384fb349edf1003f46f7667ba1903
Tower He <towerhe@gmail.com>
2012-02-03 19:59:21 +0800

    Created factories for app

---
+++ spec/factories/rightnow_oms_factory.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :cart, :class => RightnowOms::Cart do
+  end
+
+  factory :cart_item, :class => RightnowOms::CartItem do
+    association :cart, factory: :cart
+    association :cartable, factory: :product
+
+    name { cartable.name }
+    price { cartable.price }
+    quantity 1
+  end
+end
