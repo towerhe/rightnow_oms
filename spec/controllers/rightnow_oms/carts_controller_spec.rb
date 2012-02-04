ef8270bf1890e97762afabe9781cea8ceca113e3
Tower He <towerhe@gmail.com>
2012-02-04 18:19:08 +0800

    Support to remove the cart

--- spec/controllers/rightnow_oms/carts_controller_spec.rb
+++ spec/controllers/rightnow_oms/carts_controller_spec.rb
@@ -13,4 +13,18 @@ describe RightnowOms::CartsController do
       it { should respond_with_content_type /json/ }
     end
   end
+
+  describe 'DELETE destroy' do
+    let(:cart) { FactoryGirl.create(:cart) }
+
+    before do
+      RightnowOms::Cart.should_receive(:find_by_id).and_return(cart)
+      cart.should_receive(:destroy)
+
+      delete :destroy, format: :json, use_route: :rightnow_oms
+    end
+
+    it { should respond_with :ok }
+    it { should respond_with_content_type /json/ }
+  end
 end
