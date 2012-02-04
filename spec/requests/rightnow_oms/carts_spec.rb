0b4921c125bb383b41709282bb9bfca92049f593
Tower He <towerhe@gmail.com>
2012-02-04 17:12:57 +0800

    Just build the cart when it does not exist

--- spec/requests/rightnow_oms/carts_spec.rb
+++ spec/requests/rightnow_oms/carts_spec.rb
@@ -15,7 +15,7 @@ describe "Carts" do
         subject { response }
 
         it "creates a new cart" do
-          RightnowOms::Cart.all.should have(1).item
+          RightnowOms::Cart.all.should have(0).items
         end
 
         its(:status) { should == 200 }
