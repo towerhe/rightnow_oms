db1a8fdf94e06f904bf30e04edd9423ed22ffd70
Tower He <towerhe@gmail.com>
2012-02-04 17:33:13 +0800

    Saved the new cart when first adding item

--- spec/controllers/rightnow_oms/cart_items_controller_spec.rb
+++ spec/controllers/rightnow_oms/cart_items_controller_spec.rb
@@ -4,23 +4,21 @@ describe RightnowOms::CartItemsController do
   let(:product) { FactoryGirl.build(:product) }
 
   describe "POST create" do
-    let(:cart) { FactoryGirl.create(:cart) }
-    let(:cart_item) { FactoryGirl.build(:cart_item, cart: cart) }
+    context "when the cart is saved" do
+      let(:cart) { FactoryGirl.create(:cart) }
+      let(:cart_item) { FactoryGirl.build(:cart_item, cart: cart) }
 
-    context "in json" do
       before do
         RightnowOms::Cart.should_receive(:find_by_id).and_return(cart)
         controller.should_receive(:find_cartable).and_return(product)
         cart.should_receive(:add_item).with(product).and_return(cart_item)
       end
 
-      let(:format) { :json }
-
       context "with valid params" do
         before do
           cart_item.should_receive(:save).and_return(true)
 
-          post :create, format: format, use_route: :rightnow_oms
+          post :create, format: :json, use_route: :rightnow_oms
         end
 
         it { should respond_with :created }
@@ -31,10 +29,28 @@ describe RightnowOms::CartItemsController do
         before do
           cart_item.should_receive(:save).and_return(false)
 
-          post :create, format: format, use_route: :rightnow_oms
+          post :create, format: :json, use_route: :rightnow_oms
         end
 
         it { should respond_with :unprocessable_entity }
+      end
+    end
+
+    context "when the cart is not saved" do
+      let(:cart) { FactoryGirl.build(:cart) }
+      let(:cart_item) { FactoryGirl.build(:cart_item) }
+
+      before do
+        RightnowOms::Cart.should_receive(:find_by_id).and_return(cart)
+      end
+
+      it 'saves the cart' do
+        cart.should_receive(:new_record?).and_return(true)
+        cart.should_receive(:save)
+        controller.should_receive(:find_cartable).and_return(product)
+        cart.should_receive(:add_item).with(product).and_return(cart_item)
+
+        post :create, format: :json, use_route: :rightnow_oms
       end
     end
   end
