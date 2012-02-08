24cbc9d9515342f5f9c13d2fe1bffae82bbed9be
Tower He <towerhe@gmail.com>
2012-02-08 17:59:57 +0800

    Removed jquery and jquery-ujs

--- app/assets/javascripts/rightnow_oms/application.js
+++ app/assets/javascripts/rightnow_oms/application.js
@@ -1,13 +1,8 @@
-//= require jquery
-//= require jquery_ujs
 //= require_tree ./lib
+//
 //= require ./app/app
-//
-// Require models
-//
 //= require ./app/models/cart_item
 //= require ./app/models/cart
-//
 //= require_tree ./app/controllers
 //= require_tree ./app/views
 //= require_tree ./app/templates
