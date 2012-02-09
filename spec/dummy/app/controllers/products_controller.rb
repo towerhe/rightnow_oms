70e09b5da6da9e5cd76d110c6075cf02bf11b60a
Tower He <towerhe@gmail.com>
2012-02-09 15:56:17 +0800

    Load the cart using load_or_create_cart

--- spec/dummy/app/controllers/products_controller.rb
+++ spec/dummy/app/controllers/products_controller.rb
@@ -1,13 +1,7 @@
 class ProductsController < ApplicationController
+  before_filter :load_or_create_cart, only: :index
+
   def index
-    @cart = RightnowOms::Cart.find_by_id(session[:cart_id]) || RightnowOms::Cart.new
-
-    # TODO To be removed
-    if @cart.new_record?
-      @cart.save
-      session[:cart_id] = @cart.id
-    end
-
     @products = Product.all
 
     respond_to do |format|
