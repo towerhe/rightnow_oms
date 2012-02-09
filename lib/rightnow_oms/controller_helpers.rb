2d51482ffd7efafaead36adb3c355b3c566f1f1d
Tower He <towerhe@gmail.com>
2012-02-09 21:30:33 +0800

    Define the instance methods directly

--- lib/rightnow_oms/controller_helpers.rb
+++ lib/rightnow_oms/controller_helpers.rb
@@ -2,23 +2,20 @@ module RightnowOms
   module ControllerHelpers
     extend ActiveSupport::Concern
 
-    module InstanceMethods
-      def has_cart?
-        !@cart.nil?
+    def has_cart?
+      !@cart.nil?
+    end
+
+    def load_or_create_cart
+      load_cart
+
+      unless has_cart?
+        @cart = RightnowOms::Cart.create! and session[:cart_id] = @cart.id
       end
+    end
 
-      def load_or_create_cart
-        load_cart
-
-        unless has_cart?
-          @cart = RightnowOms::Cart.create! and session[:cart_id] = @cart.id
-        end
-      end
-
-      def load_cart
-        @cart = RightnowOms::Cart.find_by_id(session[:cart_id])
-      end
-
+    def load_cart
+      @cart = RightnowOms::Cart.find_by_id(session[:cart_id])
     end
   end
 end
