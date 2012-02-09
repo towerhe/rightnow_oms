23a44e665de05b759debe56c072f3399cdd2845c
Tower He <towerhe@gmail.com>
2012-02-09 15:53:31 +0800

    Extended ActionController::Base to provide methods to manage the cart

---
+++ lib/rightnow_oms/controller_helpers.rb
@@ -0,0 +1,24 @@
+module RightnowOms
+  module ControllerHelpers
+    extend ActiveSupport::Concern
+
+    module InstanceMethods
+      def has_cart?
+        !@cart.nil?
+      end
+
+      def load_or_create_cart
+        load_cart
+
+        unless has_cart?
+          @cart = Cart.create! and session[:cart_id] = @cart.id
+        end
+      end
+
+      def load_cart
+        @cart = Cart.find_by_id(session[:cart_id])
+      end
+
+    end
+  end
+end
