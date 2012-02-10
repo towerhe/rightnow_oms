e761a3bf34ea98269707ea93c7d41bccf8619353
Tower He <towerhe@gmail.com>
2012-02-10 18:10:35 +0800

    Appended a before filter to remove the null params

--- app/controllers/rightnow_oms/cart_items_controller.rb
+++ app/controllers/rightnow_oms/cart_items_controller.rb
@@ -1,5 +1,6 @@
 module RightnowOms
   class CartItemsController < ApplicationController
+    before_filter :remove_null_params
     before_filter :load_or_create_cart, only: [:index, :create]
     before_filter :load_cart_item, only: [:update, :destroy]
 
