aaa24254387826a9e5a5dba90fa91667f893eb0b
Tower He <towerhe@gmail.com>
2012-02-13 22:41:00 +0800

    Use the customized price if it is set

--- app/models/rightnow_oms/cart.rb
+++ app/models/rightnow_oms/cart.rb
@@ -19,24 +19,20 @@ module RightnowOms
     end
 
     def add_item(cartable, opts = { quantity: 1 })
-      quantity = opts[:quantity]
-      group = opts[:group]
-      parent_id = opts[:parent_id]
-
       cart_item = cart_items.find_by_cartable(cartable)
 
       if cart_item
         cart_item.update_attributes(
-          quantity: cart_item.quantity + quantity.to_i
+          quantity: cart_item.quantity + opts[:quantity].to_i,
         )
       else
         cart_item = cart_items.create(
           cartable: cartable,
           name: cartable.cartable_name,
-          price: cartable.cartable_price,
-          quantity: quantity,
-          group: group,
-          parent_id: parent_id
+          price: opts[:price] || cartable.cartable_price,
+          quantity: opts[:quantity],
+          group: opts[:group],
+          parent_id: opts[:parent_id]
         )
       end
 
