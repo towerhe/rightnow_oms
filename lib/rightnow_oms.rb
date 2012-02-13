d1e354933cb1ded207c80fb65fc122228423409b
Tower He <towerhe@gmail.com>
2012-02-13 13:37:40 +0800

    Added a validator for cartable

--- lib/rightnow_oms.rb
+++ lib/rightnow_oms.rb
@@ -1,6 +1,7 @@
 require "acts_as_api"
 
 require "rightnow_oms/engine"
+require "rightnow_oms/cartable_validator"
 require "rightnow_oms/acts_as_cartable"
 require "rightnow_oms/controller_helpers"
 
