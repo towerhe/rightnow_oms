0e376b5b6662ab48aa0d1b3d894ba0ea119e5905
Tower He <towerhe@gmail.com>
2012-02-14 16:31:32 +0800

    Rename line_items to order_items

--- spec/support/fakers.rb
+++ spec/support/fakers.rb
@@ -7,7 +7,7 @@ def fake_order_hash
       province: 'Beijing', city: Faker::Address.city, district: 'Haidian', street: Faker::Address.street_name, neighborhood: Faker::Address.neighborhood, room: fake_room_no,
       receiver: Faker::Name.name, mobile: Faker::PhoneNumber.phone_number, tel: Faker::PhoneNumber.phone_number,
       payment_mode: 'alipay', remarks: Faker::Lorem.paragraph, vbrk: Faker::Company.name,
-      line_items: [{
+      order_items: [{
         name: Faker::Product.name, price: rand(100), quantity: rand(5), parent_id: 'null',
         name: Faker::Product.name, price: rand(100), quantity: rand(5), parent_id: 'null'
       }]
