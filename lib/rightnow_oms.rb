c05121f2ea035951047cd2e83bb9b12fcf047cbe
Tower He <towerhe@gmail.com>
2012-02-15 12:33:10 +0800

    Created a default generator of order no

--- lib/rightnow_oms.rb
+++ lib/rightnow_oms.rb
@@ -5,6 +5,7 @@ require 'rightnow_oms/engine'
 require 'rightnow_oms/cartable_validator'
 require 'rightnow_oms/acts_as_cartable'
 require 'rightnow_oms/controller_helpers'
+require 'rightnow_oms/order_no_generator'
 
 module RightnowOms
   @@config = ::Confstruct::Configuration.new
