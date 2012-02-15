77def1f10cf1218978a33d2ac2c372c19582a6f0
Tower He <towerhe@gmail.com>
2012-02-15 13:57:11 +0800

    Created the order before tests

--- spec/lib/rightnow_oms/order_no_generator_spec.rb
+++ spec/lib/rightnow_oms/order_no_generator_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 
 module RightnowOms
   describe OrderNoGenerator do
-    let(:order) do
-      o =  Order.new_with_items(fake_order_hash, [fake_order_item_hash])
-      o.save
-      
-      o
-    end
-
     describe '#generate' do
-      let(:generator) { OrderNoGenerator.new(order) }
+      before do
+        @order =  Order.new_with_items(fake_order_hash, [fake_order_item_hash])
+        @order.save
+
+        @generator = OrderNoGenerator.new(@order)
+      end
+
 
       it 'generates a valid No' do
-        generator.generate.should == ('%s%06d' % [order.created_at.strftime('%Y%m%d'), order.id])
+        @generator.generate.should == ('%s%06d' % [@order.created_at.strftime('%Y%m%d'), @order.id])
       end
     end
   end
