bca7bf244bb2e94a5f73039d2d836da2ae31d1a6
Tower He <towerhe@gmail.com>
2012-02-17 12:08:27 +0800

    Fixed assertion errors

--- spec/requests/rightnow_oms/add_cart_items_spec.rb
+++ spec/requests/rightnow_oms/add_cart_items_spec.rb
@@ -5,13 +5,15 @@ feature "Add cart items to cart", js: true do
   describe "which is a singleton" do
     context "when cartable does not exists" do
       background(:all) do
+        DatabaseCleaner.strategy = nil
         FactoryGirl.create(:product, name: 'product', price: 3)
       end
 
-      after(:all) { Product.destroy_all }
+      after(:all) do
+        Product.destroy_all
 
-      before { DatabaseCleaner.strategy = nil }
-      after  { DatabaseCleaner.strategy = :truncation }
+        DatabaseCleaner.strategy = :truncation
+      end
 
       scenario "add cartable to cart" do
         visit '/products'
@@ -20,21 +22,24 @@ feature "Add cart items to cart", js: true do
         visit '/products'
 
         page.find('#rightnow-oms').should have_cart(cartable_count: 1, total: 3.0)
-        page.find('#rightnow-oms').find('table').should have_cart_items([{
+        page.find('#rightnow-oms').find('dl').should have_cart_items([{
           name: 'product', price: 3.0, quantity: 1, deletable: true
         }])
       end
     end
 
-    describe "when cartable exists" do
+    context "when cartable exists" do
       background(:all) do
+        DatabaseCleaner.strategy = nil
+
         FactoryGirl.create(:product, name: 'product', price: 3)
       end
 
-      after(:all) { Product.destroy_all }
+      after(:all) do
+        Product.destroy_all
 
-      before { DatabaseCleaner.strategy = nil }
-      after  { DatabaseCleaner.strategy = :truncation }
+        DatabaseCleaner.strategy = :truncation
+      end
 
       scenario "add cartable to cart" do
         visit '/products'
@@ -44,84 +49,79 @@ feature "Add cart items to cart", js: true do
         visit '/products'
 
         page.find('#rightnow-oms').should have_cart(cartable_count: 2, total: 6.0)
-        page.find('#rightnow-oms').find('table').should have_cart_items([{
+        page.find('#rightnow-oms dl').should have_cart_items([{
           name: 'product', price: 3.0, quantity: 2, deletable: true
         }])
       end
     end
   end
 
-  describe "when cartable does not exists and has children" do
-    context "and adding cartable with children" do
-      background(:all) do
-        @parent_product = FactoryGirl.create(:product, name: 'parent', price: 1)
-        @child_product  = FactoryGirl.create(:product, name: 'child', price: 2, parent: @parent_product)
-      end
+  describe "and adding cartable with children" do
+    background(:all) do
+      DatabaseCleaner.strategy = nil
 
-      after(:all) do
-        Product.destroy_all
-      end
+      @parent_product = FactoryGirl.create(:product, name: 'parent', price: 2)
+      @child_product  = FactoryGirl.create(:product, name: 'child', price: 2, parent: @parent_product)
+    end
 
-      before { DatabaseCleaner.strategy = nil }
-      after  { DatabaseCleaner.strategy = :truncation }
+    after(:all) do
+      Product.destroy_all
 
-      scenario "not expanded" do
-        visit '/products'
-        find('button').click
+      DatabaseCleaner.strategy = :truncation
+    end
 
-        wrapper = page.find('#rightnow-oms')
-        wrapper.should have_cart(cartable_count: 1, total: 3.0)
-        table = wrapper.find('table')
-        wrapper.find('.r-cart-bar').trigger('hover')
-        table.should have_cart_item({
-          name: 'parent', price: 1.0, quantity: 1
-        })
-        table.should_not have_cart_item({
-          name: 'child', price: 2.0, quantity: 1
-        })
+    scenario "not expanded" do
+      visit '/products'
+      find('button').click
 
-        visit '/products'
+      page.find('#rightnow-oms').should have_cart(cartable_count: 1, total: 2.0)
+      page.find('.r-cart-items dl').should have_cart_item({
+        name: 'parent', price: 2.0, quantity: 1, deletable: true
+      })
+      page.find('.r-cart-items dl').should_not have_cart_item({
+        name: 'child', price: 2.0, quantity: 1
+      })
 
-        page.find('#rightnow-oms').should have_cart(cartable_count: 1, total: 3.0)
-        table = page.find('#rightnow-oms table')
-        page.find('.r-cart-bar').trigger('hover')
-        table.should have_cart_item({
-          name: 'parent', price: 1.0, quantity: 1
-        })
-        table.should_not have_cart_item({
-          name: 'child', price: 2.0, quantity: 1
-        })
-      end
+      visit '/products'
 
-      context "and adding cartable with children" do
-        background(:all) do
-          @parent_product = FactoryGirl.create(:product, name: 'parent', price: 1)
-          @child_product  = FactoryGirl.create(:product, name: 'child', price: 2, parent: @parent_product)
-        end
+      page.find('#rightnow-oms').should have_cart(cartable_count: 1, total: 2.0)
+      page.find('.r-cart-items dl').should have_cart_item({
+        name: 'parent', price: 2.0, quantity: 1, deletable: true
+      })
+      page.find('.r-cart-items dl').should_not have_cart_item({
+        name: 'child', price: 2.0, quantity: 1
+      })
+    end
+  end
 
-        after(:all) do
-          Product.destroy_all
-        end
+  describe "and adding cartable with children" do
+    background(:all) do
+      DatabaseCleaner.strategy = nil
 
-        before { DatabaseCleaner.strategy = nil }
-        after  { DatabaseCleaner.strategy = :truncation }
+      @parent_product = FactoryGirl.create(:product, name: 'parent', price: 2)
+      @child_product  = FactoryGirl.create(:product, name: 'child', price: 2, parent: @parent_product)
+    end
 
-        scenario 'expanded' do
-          visit '/products'
-          find('button').click
+    after(:all) do
+      Product.destroy_all
 
-          table = page.find('#rightnow-oms table')
-          page.execute_script("$('.r-cart-items').css('display', 'block');")
-          find('td', text: 'parent').click
+      DatabaseCleaner.strategy = :truncation
+    end
 
-          table.should have_cart_item({
-            name: 'parent', price: 1.0, quantity: 1
-          })
-          table.should have_cart_item({
-            name: 'child', price: 2.0, quantity: 1
-          })
-        end
-      end
+    scenario 'expanded' do
+      visit '/products'
+      find('button').click
+
+      dl = page.find('#rightnow-oms dl')
+      page.execute_script("$('.r-cart-items').css('display', 'block');")
+      find('.r-cart-items li', text: 'parent').click
+
+      dl.should have_cart_item({
+        name: 'parent', price: 2.0, quantity: 1, deletable: true
+      })
+      dl.should have_cart_item({
+        name: 'child', price: 2.0, quantity: 1
+      })
     end
   end
 end
