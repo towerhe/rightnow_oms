5b2461de0fecc33dd6c716f37dc631ea0afd36ed
Tower He <towerhe@gmail.com>
2012-03-02 18:29:30 +0800

    Added tel to the order factory

--- spec/factories/rightnow_oms_factory.rb
+++ spec/factories/rightnow_oms_factory.rb
@@ -19,13 +19,14 @@ FactoryGirl.define do
     room { '%02d%02d' % [rand(20), rand(20)] }
     receiver { Faker::Name.name }
     mobile { Faker::PhoneNumber.phone_number }
+    tel { Faker::PhoneNumber.phone_number }
     payment_mode 'alipay'
     required_arrival_time { Time.now + 2.hours }
     user_id { rand(10) + 1 }
   end
 
   factory :order_item, class: RightnowOms::OrderItem do
-    association :order, factory: :order
+    association :order
 
     name { Faker::Product.name }
     price { rand(100) }
