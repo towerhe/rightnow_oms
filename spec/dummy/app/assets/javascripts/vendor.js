a2f5d17eefeaf27dc31c091ef7fafdcc336fa789
Tower He <towerhe@gmail.com>
2012-03-05 01:21:52 +0800

    Include cldr and ember-i18n

--- spec/dummy/app/assets/javascripts/vendor.js
+++ spec/dummy/app/assets/javascripts/vendor.js
@@ -3,5 +3,7 @@
 //
 //= require rightnow_oms/vendor/ember
 //= require rightnow_oms/vendor/ember-data
+//= require rightnow_oms/vendor/cldr
+//= require rightnow_oms/vendor/ember-i18n
 
 $(function() { Ember.$('#qunit-fixture').html('<div id="rightnow-oms"></div>') });
