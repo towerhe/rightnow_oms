dbccabbef691e49a09088e870dddc13d0c78a660
Tower He <towerhe@gmail.com>
2012-03-05 01:43:29 +0800

    Fixed failed tests

--- spec/support/matchers/cart_matchers.rb
+++ spec/support/matchers/cart_matchers.rb
@@ -3,9 +3,9 @@ RSpec::Matchers.define :have_cart do |cart|
   match do |actual|
     actual.trigger('hover')
 
-    actual.has_content?("购物车 #{cart[:cartable_count]} 件") &&
+    actual.has_content?("购物车 (#{cart[:cartable_count]})") &&
       actual.has_content?("查看我的购物车") &&
-      actual.has_content?("总计：￥#{cart[:total].round(2)}")
+      actual.has_content?("总计 ￥#{cart[:total].round(2)}")
   end
 end
 
