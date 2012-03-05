2288c8ef77933dcae53bb43d252f604ea28f66b9
Tower He <towerhe@gmail.com>
2012-03-05 21:44:18 +0800

    I18n support

--- spec/support/matchers/cart_matchers.rb
+++ spec/support/matchers/cart_matchers.rb
@@ -3,9 +3,9 @@ RSpec::Matchers.define :have_cart do |cart|
   match do |actual|
     actual.trigger('hover')
 
-    actual.has_content?("购物车 (#{cart[:cartable_count]})") &&
-      actual.has_content?("查看我的购物车") &&
-      actual.has_content?("总计 ￥#{cart[:total].round(2)}")
+    actual.has_content?("Shopping Cart (#{cart[:cartable_count]})") &&
+      actual.has_content?("Check Cart") &&
+      actual.has_content?("Total $#{cart[:total].round(2)}")
   end
 end
 
@@ -13,7 +13,6 @@ RSpec::Matchers.define :have_cart_item do |item|
   match do |actual|
     matched = actual.has_content?(item[:name]) &&
       actual.has_content?("#{'%.2f' % item[:price]}x#{item[:quantity]}")
-    #matched &&= wrapper.has_content?('删除') if item[:deletable]
 
     matched
   end
